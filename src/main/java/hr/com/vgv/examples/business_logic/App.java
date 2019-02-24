f92a0ac60b17beb6b53cf5fbd50f45bce96bbaf9
Vatavuk <vatavuk1@hotmail.com>
2019-02-24 20:52:03 +0100

    http server using java sockets

--- src/main/java/hr/com/vgv/examples/business_logic/App.java
+++ src/main/java/hr/com/vgv/examples/business_logic/App.java
@@ -1,11 +1,32 @@
 package hr.com.vgv.examples.business_logic;
 
-import java.util.ArrayList;
-import java.util.Properties;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
 
 public class App {
 
     public static void main(String... args) throws Exception {
-        Properties props = System.getProperties();
+        final Integer port = 8080;
+        System.out.println("Server started on port " + port);
+
+        try(ServerSocket server = new ServerSocket(port)) {
+            while(true) {
+                try (Socket socket = server.accept();
+                     InputStream input = socket.getInputStream();
+                     OutputStream output = socket.getOutputStream()
+                ) {
+
+                    byte[] buffer = new byte[10000];
+                    int size = input.read(buffer);
+                    String request = new String(Arrays.copyOfRange(buffer, 0, size));
+                    System.out.print(request);
+
+                    output.write("HTTP/1.1 200 OK\r\n\r\nHello World!".getBytes());
+                }
+            }
+        }
     }
 }
