6d85a24ec3e01879a3c596fcf8548b72fccb34d6
Vatavuk <vatavuk1@hotmail.com>
2019-02-25 21:49:06 +0100

    http request parsing

--- src/main/java/hr/com/vgv/examples/httpserver/App.java
+++ src/main/java/hr/com/vgv/examples/httpserver/App.java
@@ -5,6 +5,9 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class App {
 
@@ -23,10 +26,33 @@ public class App {
                     int size = input.read(buffer);
                     String request = new String(Arrays.copyOfRange(buffer, 0, size + 1));
                     System.out.print(request);
+                    if (!request.isEmpty()) {
+                        StringTokenizer tokenizer = new StringTokenizer(request);
+                        String method = tokenizer.nextToken();
+                        String path = tokenizer.nextToken();
 
-                    output.write("HTTP/1.1 200 OK\r\n\r\nHello World!".getBytes());
+                        if (method.equals("GET") &&
+                            matches("/world/\\w+", path))
+                        {
+                            String id = path.split("/")[2];
+                            System.out.println(id);
+                            output.write("HTTP/1.1 200 OK\r\n\r\nHello World!".getBytes());
+                        } else {
+                            output.write("HTTP/1.1 400 BAD REQUEST\r\n\r\n".getBytes());
+                        }
+                    }
                 }
             }
         }
+    }
+
+    private static boolean matches(String regex, String text) {
+        //"/world/w+"
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        if (matcher.find()){
+            return true;
+        }
+        return false;
     }
 }
